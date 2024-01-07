cabd1a0beab4d45651b2e914412af29821d875be
Saransh <saranshseth93@gmail.com>
2024-01-07 21:47:23 +1100

    3D Portfolio start with pokemon land

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -70,9 +70,9 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <directionalLight position={[1, 1, 1]} intensity={4} />
           <ambientLight intensity={0.5} />
-          <pointLight position={[10, 5, 10]} intensity={2} />
+          <pointLight position={[10, 5, 10]} intensity={4} />
           <spotLight
             position={[0, 50, 10]}
             angle={0.15}
